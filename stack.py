9f58c1189a5326a93940d79c7a1bd8ef7952dd65
Felix Sawicki <fmsawicki@gmail.con>
2018-11-01 09:54:35 -0400

    wrote out stack class, methods, and dummy values. working on testing

---
+++ stack.py
@@ -0,0 +1,12 @@
+class Stack:
+    def __init__(self, arr):
+        self.arr = arr
+
+        def add(self, arr):
+           x = input("What would you like to add to the stack?")
+           arr.insert(0, x)
+
+        def take(self, arr):
+         arr.pop(0)
+
+arr = ["Test1", "Test2", "Test3"]
